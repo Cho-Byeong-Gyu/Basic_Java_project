5a9d90d2e7d19c1a91a3c10f6c52f23ade653188
Cho-Byeong-Gyu <160321523+Cho-Byeong-Gyu@users.noreply.github.com>
2024-03-15 11:43:35 +0900

    Add files via upload

---
+++ printf함수_예제1.java
@@ -0,0 +1,13 @@
+package printf함수_예제1;
+
+public class printf함수_예제1 {
+	public static void main(String[] args) {
+		
+		int finger = 10;
+		
+		System.out.printf("finger = [%5d]  \n", finger);
+		System.out.printf("finger = [%-5d] \n", finger);
+		System.out.printf("finger = [%05d] \n", finger);
+		
+	}
+}
