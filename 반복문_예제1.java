1d3664cfdaa53835a4beac284c7d60bc462891c5
Cho-Byeong-Gyu <160321523+Cho-Byeong-Gyu@users.noreply.github.com>
2024-03-22 20:36:48 +0900

    Add files via upload

---
+++ 반복문_예제1.java
@@ -0,0 +1,13 @@
+package 반복문_예제1;
+
+public class 반복문_예제1 {
+
+	public static void main(String[] args) {
+		for(int i=1, j=10; i<=10; i++,j--) {	// 조건식 변수2개 동시선언 + 10번 반복하는 for문
+			System.out.printf("%d 	%d \n", i,j);
+		}
+
+		
+	}
+
+}
